af95fd2ef41aeaeee5b81ac829245db4197d9afe
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-11-13 22:16:15 -0600

    user data entered in browser made it to controller

--- 111316_FSUser/fsuser/public/controllers/controller.js
+++ 111316_FSUser/fsuser/public/controllers/controller.js
@@ -1,11 +1,13 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-    console.log("Hello World from controller");
+  console.log("Hello World from controller");
 
-$http.get('/fsuser').success(function(response){
-  console.log("I got the data I requested");
-  $scope.fsuser = response;
-});
+  $http.get('/fsuser').success(function(response){
+    console.log("I got the data I requested");
+    $scope.fsuser = response;
+  });
 
-
+  $scope.addUser = function(){
+    console.log($scope.user);
+  };
 }]);
