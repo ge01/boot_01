74d6f289507e6e14f6e37885a6efad91c8762263
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-11-13 21:08:31 -0600

    server.js received a get request

--- 111316_FSUser/fsuser/public/controllers/controller.js
+++ 111316_FSUser/fsuser/public/controllers/controller.js
@@ -1,4 +1,18 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
+
+$http.get('/fsuser');
+
+    person1 = {
+      name: 'Tim',
+      email: 'tim@email.com'
+    };
+    person2 = {
+      name: 'Emily',
+      email: 'emily@email.com'
+    };
+
+    var fsuser = [person1, person2];
+    $scope.fsuser = fsuser;
 }]);
