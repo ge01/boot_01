72709c80229d1e737478dbc0e2e91261f9d1ee44
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-11-13 21:44:36 -0600

    data travelled from db to server through controller to the view

--- 111316_FSUser/fsuser/server.js
+++ 111316_FSUser/fsuser/server.js
@@ -1,22 +1,17 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('fsuser', ['fsuser']);
 
 app.use(express.static(__dirname + "/public"));
 
 app.get('/fsuser', function(req, res){
   console.log("I received a GET request");
 
-  person1 = {
-    name: 'Tim',
-    email: 'tim@email.com'
-  };
-  person2 = {
-    name: 'Emily',
-    email: 'emily@email.com'
-  };
-
-  var fsuser = [person1, person2];
-  res.json(fsuser);
+  db.fsuser.find(function(err, docs){
+    console.log(docs);
+    res.json(docs);
+  });
 });
 
 app.listen(3000);
