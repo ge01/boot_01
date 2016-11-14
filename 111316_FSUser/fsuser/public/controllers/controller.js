9f217d0d4c85d30930f12d1b9a2f50a4a06f2fd5
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-11-13 21:16:06 -0600

    browser received the data from the server

--- 111316_FSUser/fsuser/public/controllers/controller.js
+++ 111316_FSUser/fsuser/public/controllers/controller.js
@@ -2,17 +2,10 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
 
-$http.get('/fsuser');
+$http.get('/fsuser').success(function(response){
+  console.log("I got the data I requested");
+  $scope.fsuser = response;
+});
 
-    person1 = {
-      name: 'Tim',
-      email: 'tim@email.com'
-    };
-    person2 = {
-      name: 'Emily',
-      email: 'emily@email.com'
-    };
 
-    var fsuser = [person1, person2];
-    $scope.fsuser = fsuser;
 }]);
--- 111316_FSUser/fsuser/server.js
+++ 111316_FSUser/fsuser/server.js
@@ -5,6 +5,18 @@ app.use(express.static(__dirname + "/public"));
 
 app.get('/fsuser', function(req, res){
   console.log("I received a GET request");
+
+  person1 = {
+    name: 'Tim',
+    email: 'tim@email.com'
+  };
+  person2 = {
+    name: 'Emily',
+    email: 'emily@email.com'
+  };
+
+  var fsuser = [person1, person2];
+  res.json(fsuser);
 });
 
 app.listen(3000);
