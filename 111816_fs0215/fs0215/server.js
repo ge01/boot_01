1ed4281e0d0654811675475272bf3edeb3a38d8d
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-11-18 14:51:26 -0600

    111816

--- 111816_fs0215/fs0215/server.js
+++ 111816_fs0215/fs0215/server.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var app = express();
 
-// Set the route to our index page
-app.get('/', function(req, res){
-  res.send("Hello world from server.js");
-});
+app.use(express.static(__dirname + "/public"));
 
 app.listen(3000);
 console.log("Server running on port 3000");
