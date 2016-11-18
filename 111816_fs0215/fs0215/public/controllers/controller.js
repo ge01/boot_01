09e0857bd492a2563051de37492a1a76a3eb0b4b
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-11-18 17:26:22 -0600

    fs0215 02_111816

--- 111816_fs0215/fs0215/public/controllers/controller.js
+++ 111816_fs0215/fs0215/public/controllers/controller.js
@@ -1,4 +1,13 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
+
+  $http.get('/fs0215').success(function(response){
+    console.log("I got the data I requested");
+    $scope.fs0215 = response;
+  });
+
+
+
+
 }]);
--- 111816_fs0215/fs0215/server.js
+++ 111816_fs0215/fs0215/server.js
@@ -3,5 +3,18 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/fs0215', function(req, res){
+  console.log("I received a GET request");
+
+  data1 = {
+    originalPrice: "100",
+    discount: "0.2",
+    salePrice: "80"
+  };
+
+  var fs0215 = [data1];
+  res.json(fs0215);
+});
+
 app.listen(3000);
 console.log("Server running on port 3000");
